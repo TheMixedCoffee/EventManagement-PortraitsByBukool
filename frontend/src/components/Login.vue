956b9711346939179385e0ef158c6f6861b2ad65
Marc Lloyd Quisel <62422987+marqueesel@users.noreply.github.com>
2020-12-27 21:12:18 +0800

    designed login and register

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,21 +1,36 @@
 <template>
-  <div class="container">
-    <i class="far fa-calendar-alt"></i>
-    <div class="form-group">
-      <label class="label">Username</label>
-      <div class="control">
-        <input class="form-control" type="text" placeholder="Enter username" v-model="username"/>
-      </div>
-    </div>
+  <div class="bg">
+    <div class="container">
+      <link rel="stylesheet" href="../pages/css/about.css">
+      <div class="row mt-5">
+        <div class="col-sm-5 bg-dark border py-5 text-center">
+          <img src="../../public/pages/img/sunlogo(gray).png" alt="" class="w-50">
+          <h3 class="text-light poiret">Portraits by Bukool</h3>
+          <br>
+          <br>
+          <h1 class="ramaraja" style="color: #A8A8A8;">Welcome Back</h1>
+          <p style="color: #A8A8A8;">To help connected with us please login <br> with your personal info</p>
+        </div>
+        <div class="col-sm-7 p-5 align-self-center">
+          <h1 class="ramaraja text-dark text-center">Login to your Account</h1>
+          <div class="form-group">
+            <label class="label">Username</label>
+            <div class="control">
+              <input class="form-control" type="text" placeholder="Enter your username" v-model="username"/>
+            </div>
+          </div>
 
-    <div class="form-group">
-      <label class="label">Password</label>
-      <div class="control">
-        <input class="form-control" type="password" placeholder="Enter password" v-model="password"/>
+          <div class="form-group">
+            <label class="label">Password</label>
+            <div class="control">
+              <input class="form-control" type="password" placeholder="Enter your password" v-model="password"/>
+            </div>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary" @click="loginUser">LOGIN</button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-primary" @click="loginUser">LOGIN</button>
     </div>
   </div>
 </template>
